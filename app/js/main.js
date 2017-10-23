c19af268fd590a80317ad9e9c9b8aa3c9abf3970
cooltolia <cooltolia@gmail.com>
2017-10-23 11:09:22 +0300

    Сверстана основная часть блоков

--- app/js/main.js
+++ app/js/main.js
@@ -73,103 +73,27 @@
     });
 
     
-        $(document).ready(function() {
-    
-      var nextScreen = function() {
-        var questions = $('.questions');
-        var start = $('.start');
-        var trigger = start.find('a');
-    
-        trigger.on('click', function(e) {
-          e.preventDefault();
-          questions.css('transform', 'translateX(-100%)');
-        });
-    
-      }();
-    
-      var data_array = [
-        ["Сколько сантиметров в метре?","10","1024","100","1000",3],
-        ["Перевод слова: Hello","Как дела?","Привет","Ты","Дом",2],
-        ["Перевод слова: Dog","Собака","Кошка","Дерево","Сосиска",1],
-        ["Сколько месяцев в году?","10","11","12","13",3],
-        ["Перевод слова: Tree","Три","Собака","Дерево","Дом",3],
-        ["Перевод слова: Wall","Стена","Дом","Башня","Война",1],
-      ];
-    
-      var images_array = [
-        "images/q1.jpg", "images/q2.jpg", "images/q3.jpg", "images/q4.jpg", "images/q5.jpg", "images/q6.jpg"
-      ]
-    
-      var currentQuestion = 0;
-      var questionsNumber = 1;
-      var totalQiestions = data_array.length;
-      var result = 0;
-    
-      var $startBtn =$(".start__btn");
-      var $question = $(".questions__title");
-      var $image = $(".questions__image img")
-      var $number = $(".questions__number");
-      var $answer = $(".questions__answer"),
-          $answer1 = $("#answer1"),
-          $answer2 = $("#answer2"),
-          $answer3 = $("#answer3"),
-          $answer4 = $("#answer4");
-      var final = $('.final');
+
     
     
-    
-      $startBtn.on('click', function() {
-    
-        newQuestion()
-    
-      });
-    
-      function newQuestion() {
-        $question.html(data_array[currentQuestion][0]);
-        $answer1.html(data_array[currentQuestion][1]);
-        $answer2.html(data_array[currentQuestion][2]);
-        $answer3.html(data_array[currentQuestion][3]);
-        $answer4.html(data_array[currentQuestion][4]);
-        $number.html(questionsNumber + "/" + totalQiestions);
-        $image.attr("src", images_array[currentQuestion]);
-      }
-    
-      $answer.on('click', function(e){
-        var $this = $( e.target );
-        if (questionsNumber < (totalQiestions) ) {
-          console.log(questionsNumber);
-          console.log(data_array[currentQuestion][5]);
-          questionsNumber++;
-          if ($this.attr("rel") == data_array[currentQuestion][5] ) {
-    
-            console.log("correct")
-            result++;
-            currentQuestion++;
-    
-            newQuestion();
-    
-          } else {
-    
-            console.log("wrong")
-            currentQuestion++;
-            newQuestion();
-    
-          }
-    
-        } else {
-    
-          final.css('transform', 'translateX(-100%)');
-          console.log(questionsNumber);
-          console.log(result);
-        }
-    
-      });
+
     
     
+
     
-    });
     
-        
+
+    
+    
+
+    
+    
+
+    
+    
+
+    
+    
   });
 
 })(jQuery)
