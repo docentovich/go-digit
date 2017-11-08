9ad81cfb9b7f865ce65adecd33c188974118d71d
kulak <cooltolia@gmail.com>
2017-11-08 12:45:55 +0300

    правочки

--- app/js/main.js
+++ app/js/main.js
@@ -181,39 +181,127 @@
 
         $portfolioList = $(".portfolio__list"),
 
-        $portfolioItem = $(".portfolio__item:nth-of-type(n+5)"),
+        $portfolioItem = $(".portfolio__item"),
+
+        $portfolioLink = $portfolioItem.find('a'),
 
         $portfolioItemVisible = $(".portfolio__item:first-child");
 
-        $animatedHeight = $portfolioItemVisible.height() + 'px';
+    
+
+    // $portfolioBtn.on('click', function() {
+
+    //     if ($portfolioList.hasClass('active')) {
+
+    //         $portfolioLink.animate({'padding-top': '0'}, 500);
+
+    //         $portfolioList.removeClass('active');
+
+    //         $( this ).html('Показать все проекты');
+
+    //     } else {
+
+    //         $portfolioLink.animate({'padding-top': '66%'}, 500);
+
+    //         $portfolioList.addClass('active');
+
+    //         $( this ).html('Показать меньше');
+
+    //     }
+
+    // });
+
+    
+
+    var itemHeight;
+
+    var maxHeight;
+
+    
+
+    function windowSize(){
+
+        if ($(window).width() <= '479'){
+
+            itemHeight = $portfolioItem.outerHeight(true);
+
+            maxHeight = (itemHeight * 4) + 'px';
+
+            $portfolioList.css("height", maxHeight);
+
+            console.log(maxHeight);
+
+    
+
+        } else {
+
+            itemHeight = $portfolioItem.outerHeight(true);
+
+            maxHeight = (itemHeight * 2) + 'px';
+
+            $portfolioList.css("height", maxHeight);
+
+            console.log(maxHeight);
+
+        }
+
+    }
+
+    $(window).on('load resize', windowSize);
+
+    
+
+    
+
+    
 
     $portfolioBtn.on('click', function() {
 
-        $portfolioItem.each(function() {
+    
 
-         var $this = $(this);
-
-         $thisHeight = $this.height();
+        if ( $portfolioList.hasClass("expanded") ) {
 
     
 
-         $this.animate({height: "toggle"}, 500);
+            $portfolioList.removeClass("expanded");
 
     
 
-        //  if ($this.css('height') == 0) {
+            $portfolioList.animate(
 
-        //         $this.animate({"height": "284px"}, 3000);
+                {height: maxHeight}, 500
 
-        //     }
+            );
 
-        //     else {
+    
 
-        //         $this.animate({"height": 0}, 3000);
+            $( this ).html('Показать все проекты');
 
-        //     }
+    
 
-        });
+        } else {
+
+    
+
+            $portfolioList.addClass("expanded");
+
+    
+
+            $portfolioList.animate({
+
+                height: $portfolioList.get(0).scrollHeight
+
+            }, 500, function(){
+
+            $(this).height('auto')});
+
+    
+
+            $( this ).html('Показать меньше');
+
+    
+
+        }
 
     });
 
--- develop/blocks/portfolio/portfolio.js
+++ develop/blocks/portfolio/portfolio.js
@@ -1,20 +1,64 @@
 var $portfolioBtn = $(".portfolio__btn"),
     $portfolioList = $(".portfolio__list"),
-    $portfolioItem = $(".portfolio__item:nth-of-type(n+5)"),
+    $portfolioItem = $(".portfolio__item"),
+    $portfolioLink = $portfolioItem.find('a'),
     $portfolioItemVisible = $(".portfolio__item:first-child");
-    $animatedHeight = $portfolioItemVisible.height() + 'px';
+
+// $portfolioBtn.on('click', function() {
+//     if ($portfolioList.hasClass('active')) {
+//         $portfolioLink.animate({'padding-top': '0'}, 500);
+//         $portfolioList.removeClass('active');
+//         $( this ).html('Показать все проекты');
+//     } else {
+//         $portfolioLink.animate({'padding-top': '66%'}, 500);
+//         $portfolioList.addClass('active');
+//         $( this ).html('Показать меньше');
+//     }
+// });
+
+var itemHeight;
+var maxHeight;
+
+function windowSize(){
+    if ($(window).width() <= '479'){
+        itemHeight = $portfolioItem.outerHeight(true);
+        maxHeight = (itemHeight * 4) + 'px';
+        $portfolioList.css("height", maxHeight);
+        console.log(maxHeight);
+
+    } else {
+        itemHeight = $portfolioItem.outerHeight(true);
+        maxHeight = (itemHeight * 2) + 'px';
+        $portfolioList.css("height", maxHeight);
+        console.log(maxHeight);
+    }
+}
+$(window).on('load resize', windowSize);
+
+
+
 $portfolioBtn.on('click', function() {
-    $portfolioItem.each(function() {
-     var $this = $(this);
-     $thisHeight = $this.height();
 
-     $this.animate({height: "toggle"}, 500);
+    if ( $portfolioList.hasClass("expanded") ) {
 
-    //  if ($this.css('height') == 0) {
-    //         $this.animate({"height": "284px"}, 3000);
-    //     }
-    //     else {
-    //         $this.animate({"height": 0}, 3000);
-    //     }
-    });
+        $portfolioList.removeClass("expanded");
+
+        $portfolioList.animate(
+            {height: maxHeight}, 500
+        );
+
+        $( this ).html('Показать все проекты');
+
+    } else {
+
+        $portfolioList.addClass("expanded");
+
+        $portfolioList.animate({
+            height: $portfolioList.get(0).scrollHeight
+        }, 500, function(){
+        $(this).height('auto')});
+
+        $( this ).html('Показать меньше');
+
+    }
 });
